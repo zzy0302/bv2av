458241a47800df96d6759162bd8d59e33171a684
zzy0302 <503652983@qq.com>
2020-03-25 12:16:25 +0800

    v0.0.2

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 - 没啥特别功能，有需要的提pr
 - GitHub：[BV2av](https://github.com/zzy0302/bv2av)
 - 油猴：[av还原助手](https://greasyfork.org/en/scripts/398640)
+
+#### 更新日志
+- v0.0.2
+修复了分p视频链接错误
+批量替换网页上所有BV链接
+
+- v0.0.1
+替换浏览器地址栏链接
--- bv2av.js
+++ bv2av.js
@@ -4,8 +4,8 @@
 // @namespace       [url=mailto:zzy0302@vip.qq.com]zzy0302@vip.qq.com[/url]
 // @author          zzy0302
 // @description     av还原助手,还原所有bv至av
-// @match           https://www.bilibili.com/video/BV*
-// @version         0.0.1
+// @match           *://*.bilibili.com/video/*
+// @version         0.0.2
 // @grant           none
 // @license         MIT License
 // ==/UserScript==
@@ -13,5 +13,33 @@
     'use strict';
     let elem = document.querySelector('#video-page-app');
     if (!elem) return;
-    history.replaceState(202, '', 'https://www.bilibili.com/video/av' + window.aid)
+    function bv2av(x) {
+        var bvs = x.match(/[bB][vV][fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF]{10}/g);
+        if (bvs) {
+            console.log(bvs)
+            x = x.replace(bvs, 'av' + window.aid);
+        }
+        return x;
+    }
+
+    var url = window.location.href;
+    var newUrl = bv2av(url);
+    if (url != newUrl) {
+        window.history.pushState(null, null, newUrl);
+    }
+
+    setInterval(function () {
+        var nodes = document.querySelectorAll('a');
+        for (var o of nodes) {
+            if (o.href) {
+                o.href = bv2av(o.href);
+            }
+        }
+        nodes = document.querySelectorAll('div');
+        for (o of nodes) {
+            if (o.title) {
+                o.title = bv2av(o.title);
+            }
+        }
+    }, 500);
 })();
